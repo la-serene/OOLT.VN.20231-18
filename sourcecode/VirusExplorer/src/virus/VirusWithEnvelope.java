3e8641321e8b1464a2d1b0882b9b3c4822e8dafd
Nguyen Cao Ky <caoky2003xx@gmail.com>
2024-01-05 11:03:44 +0700

    Initial commit

--- sourcecode/VirusExplorer/src/virus/VirusWithEnvelope.java
+++
@@ -1,7 +0,0 @@
-package virus;
-
-public class VirusWithEnvelope extends Virus {
-    public VirusWithEnvelope(String name, String ILLUSTRATION_PATH, String INFECTION_MECHANISM_PATH) {
-        super(name, ILLUSTRATION_PATH, INFECTION_MECHANISM_PATH);
-    }
-}
