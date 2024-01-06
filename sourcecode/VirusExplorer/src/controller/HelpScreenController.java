6ecd73a1220f47b8bdcb7dd1b87e4ff6cc390262
Nguyen Cao Ky <caoky2003xx@gmail.com>
2024-01-06 20:05:55 +0700

    display user guide

--- sourcecode/VirusExplorer/src/controller/HelpScreenController.java
+++ sourcecode/VirusExplorer/src/controller/HelpScreenController.java
@@ -3,10 +3,24 @@ package controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class HelpScreenController {
     @FXML
     private TextArea taAbout;
     public void setAbout() {
-        taAbout.setText("This is a guide on application use.");
+        try {
+            String HELP_PATH = "/assets/help/user_guide.txt";
+
+            String currentPath = System.getProperty("user.dir");
+            HELP_PATH = currentPath + "/src/" + HELP_PATH;
+
+            String content = new String(Files.readAllBytes(Paths.get(HELP_PATH)));
+            taAbout.setText(content);
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
     }
 }
