07ca6c6b69d7c0f715d14d93d4bd17765cc8912b
Nguyen Cao Ky <caoky2003xx@gmail.com>
2024-01-06 17:15:26 +0700

    follow class diagram

--- sourcecode/VirusExplorer/src/virus/Virus.java
+++ sourcecode/VirusExplorer/src/virus/Virus.java
@@ -1,12 +1,24 @@
 package virus;
 
 abstract public class Virus {
-    public String name;
-    public final String ILLUSTRATION_PATH;
-    public final String INFECTION_MECHANISM_PATH;
+    private String name;
+    private final String ILLUSTRATION_PATH;
+    private final String INFECTION_MECHANISM_PATH;
+
     public Virus(String name, String ILLUSTRATION_PATH, String INFECTION_MECHANISM_PATH) {
         this.name = name;
         this.ILLUSTRATION_PATH = ILLUSTRATION_PATH;
         this.INFECTION_MECHANISM_PATH = INFECTION_MECHANISM_PATH;
     }
-}
+
+    public String getName() {
+        return name;
+    }
+
+    public String getIllustrationPath() {
+        return ILLUSTRATION_PATH;
+    }
+
+    public String getInfectionMechanismPath() {
+        return INFECTION_MECHANISM_PATH;
+    }
